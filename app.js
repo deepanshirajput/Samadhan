df76f5d1fc5b0215194e16453f2889bf6cea12c3
Shreyansh Mishra <201951146@iiitvadodara.ac.in>
2023-02-21 23:49:57 +0530

    add status to response and remove unnecessary code

--- app.js
+++ app.js
@@ -55,18 +55,17 @@ app.post('/login', async (req, res) => {
                 let usrname = foundUser.username;
                 res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
             } else {
-                res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+                res.status(200).send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
             }
         }
         else {
-    
-            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
-            await bcrypt.compare(req.body.password, fakePass);
-    
-            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+            res.status(400).send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
         }
     } catch{
-        res.send("Internal server error");
+        res.status(500).send({
+            "success" : false,
+            "message" : "Internal server error"
+        });
     }
 });
 server.listen(3000, function(){
